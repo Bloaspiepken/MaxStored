3b1bb52f2efa32bd174ec2a4d583adbbdcead4fb
Rails Maxstored <philip.nijhof@icloud.com>
2016-04-18 20:08:27 +0000

    Add association user and articles and debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417183854) do
+ActiveRecord::Schema.define(version: 20160418194847) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
