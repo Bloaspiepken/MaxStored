395237e6bc836d2109130857248fc8189e3f92f7
Rails Maxstored <philip.nijhof@icloud.com>
2016-04-24 19:44:27 +0000

    Add admin for category inquiries

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -21,9 +23,15 @@ class CategoriesController < ApplicationController
     
   end
   
-    private
+  private
   def category_params
     params.require(:category).permit(:name)
   end
   
+    def require_admin
+      if !logged_in? || (logged_in? and !current_user.admin?)
+       flash[:danger] = "Alleen admin kunnen deze actie uitvoeren"
+       redirect_to categories_path
+      end
+    end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "personenwagenbanden")
+    @user = User.create(username: "philip", email: "philip@example.com", password: "password", admin: true)
   end
   
   
@@ -12,12 +13,20 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
   
-  test "should get show" do
-    get(:show, {'id' => @category.id})
-    assert_response :success
+    test "should get show" do
+      get(:show, {'id' => @category.id})
+      assert_response :success
+    end
+    
+    test "should redirect create when admin not logged in" do
+      assert_no_difference 'Category.count' do
+      post :create, category: { name: "personenwagenbanden" }
+    end
+    assert_redirected_to categories_path
   end
 end
